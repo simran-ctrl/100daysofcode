960e554fc79eb34e1baa2686401b05009eaea116
Simran Parmar <83174952+simran-ctrl@users.noreply.github.com>
2023-08-08 21:41:57 +0530

    Added solution - LeetHub

---
+++ Fraction pairs with sum 1 - GFG/fraction-pairs-with-sum-1.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+
+
+    int countFractions(int n, int numerator[], int denominator[])
+    {
+        
+     unordered_map<double,int> m;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            double x = double(denominator[i]-numerator[i])/double(denominator[i]);
+            double y = double(numerator[i])/double(denominator[i]);
+            if(m[x]){
+                count+=m[x];
+            }
+            m[y]++;
+        }
+        return count;
+   
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int numerator[n],denominator[n];
+        for(int i=0;i<n;i++)
+            cin>>numerator[i];
+        for(int i=0;i<n;i++)
+            cin>>denominator[i];
+        Solution ob;
+        int ans=ob.countFractions(n,numerator,denominator);
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
