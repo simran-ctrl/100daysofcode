f45bc6efaf1df0ce2ae6469bf104199037698ae1
Simran Parmar <83174952+simran-ctrl@users.noreply.github.com>
2023-08-10 23:27:46 +0530

    Added solution - LeetHub

---
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        //Your code here
+        //return vector with correct order of elements
+         vector<int> result;
+
+    int i = 0, j = 0;
+
+    while (i < n && j < m) {
+        if (i > 0 && arr1[i] == arr1[i - 1]) {
+            i++;
+            continue; // Skip duplicates in the first array
+        }
+
+        if (j > 0 && arr2[j] == arr2[j - 1]) {
+            j++;
+            continue; // Skip duplicates in the second array
+        }
+
+        if (arr1[i] < arr2[j]) {
+            result.push_back(arr1[i]);
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            result.push_back(arr2[j]);
+            j++;
+        } else {
+            result.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    // Add remaining elements from both arrays
+    while (i < n) {
+        if (i == 0 || arr1[i] != arr1[i - 1]) {
+            result.push_back(arr1[i]);
+        }
+        i++;
+    }
+
+    while (j < m) {
+        if (j == 0 || arr2[j] != arr2[j - 1]) {
+            result.push_back(arr2[j]);
+        }
+        j++;
+    }
+
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
