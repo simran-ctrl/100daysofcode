cbe43a012ae6f232e25383743f5999d4ae4e0249
Simran Parmar <83174952+simran-ctrl@users.noreply.github.com>
2023-08-14 23:39:16 +0530

    Added solution - LeetHub

--- Sum of AP series - GFG/sum-of-ap-series.cpp
+++ Sum of AP series - GFG/sum-of-ap-series.cpp
@@ -12,7 +12,7 @@ class Solution
 		long sum_of_ap(long n,long a, long d)
 		{
 		    // Code here.
-		     long sum = (n * (2 * a + (n - 1) * d)) / 2;
+		    long sum = (n * (2 * a + (n - 1) * d)) / 2;
     return sum;
 		}
 };
