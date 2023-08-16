22b850d1f5b736d28914ccb12208a84051fd7c95
Simran Parmar <83174952+simran-ctrl@users.noreply.github.com>
2023-08-16 19:00:14 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l,int m, int r)
+    {
+         // Your code here
+          int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int left[n1], right[n2];
+
+    for (int i = 0; i < n1; i++)
+        left[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        right[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j]) {
+            arr[k] = left[i];
+            i++;
+        } else {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+
+        
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+         if (l < r) {
+        int m = l + (r - l) / 2; // Calculate the middle index
+
+        mergeSort(arr, l, m);     // Sort first half
+        mergeSort(arr, m + 1, r); // Sort second half
+
+        merge(arr, l, m, r);      // Merge the two halves
+    }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
