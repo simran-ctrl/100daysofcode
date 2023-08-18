a6629abbad7cf96aabf6c02e5ee005ae9f3b1dce
Simran Parmar <83174952+simran-ctrl@users.noreply.github.com>
2023-08-18 23:54:53 +0530

    Added solution - LeetHub

---
+++ Sort The Array - GFG/sort-the-array.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+    public:
+    
+    vector<int> sortArr(vector<int>arr, int n){
+    //complete the function here
+     quickSort(arr, 0, n - 1);
+    return arr;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int>s(n);
+        for(int i = 0; i < n; i++)
+            cin >> s[i];
+        Solution ob;
+        vector<int>v = ob.sortArr(s, n);
+        for(auto i : v)
+            cout << i << ' ';
+        cout << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
